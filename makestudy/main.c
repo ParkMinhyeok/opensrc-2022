868897e02604ab8fe230accdddf76edf96c3569b
ParkMinhyeok <89285886+ParkMinhyeok@users.noreply.github.com>
2022-11-17 14:51:55 +0900

    Add files via upload
    makestudy upload

---
+++ makestudy/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "copy.h"
+#include <stdlib.h>
+
+int main(){
+	char* str1 = "Worldcup! Final 4";
+	char* str2 = copy(str1);
+
+	printf("%s\n", str2);
+	free(str2);
+
+	return 0;
+}
